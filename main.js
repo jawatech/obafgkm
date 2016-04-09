11867f4d1832c4cdd08271fe582c1c689515804a
jawatech <jawatech@gmail.com>
2016-04-09 11:44:06 +0800

    working version

--- main.js
+++ main.js
@@ -2,12 +2,19 @@
 const qs = require ("querystring");
 const electron = require('electron');
 const path = require('path');
+const fs = require('fs');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-const pdfURL = "http://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf";
-var webContents = null;
+//const pdfURLHOME = "http://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf";
+//const pdfURLHOME = "file://C:/Users/sig/github/pdf.js/web/compressed.tracemonkey-pldi-09.pdf";
+const pdfURLHOME = "Dropbox/org/gtd/mygtd.pdf";
+const host='http://127.0.0.1:8080/Dropbox';
+const host2='http://127.0.0.1:6968/';
+var webContents = {};
+var browserWindows = {};
 var mainWindow = null;
 const ipcMain = require('electron').ipcMain;
+
 app.on('ready', function() {
   mainWindow = new BrowserWindow({
     width: 800,
@@ -19,79 +26,110 @@ app.on('ready', function() {
       //the path of preload script has to be "absolute path".
       //https://github.com/atom/electron/blob/v0.34.3/docs/api/browser-window.md#new-browserwindowoptions
       preload: path.join(__dirname, 'preload.js')
-		      
     },
   });
-  const param = qs.stringify({file: pdfURL});
-
-  mainWindow.loadURL('file://' + __dirname + '/../pdf.js/web/viewer.html');//?' + param);
-  mainWindow.webContents.openDevTools();
-  webContents = mainWindow.webContents;
+  const param = qs.stringify({file: pdfURLHOME});
+  //mainWindow.loadURL('file://' + __dirname + '/../pdf.js/web/viewer.html?' + param);
+  //file://C:\Users\sig\github\electron-pdfjs/../pdf.js/web/viewer.html
+  mainWindow.loadURL(host+'/pdf.js/web/viewer.html?' + param);
+  console.log(host+'/pdf.js/web/viewer.html?' + param);
+  //mainWindow.webContents.openDevTools();
+  webContents[pdfURLHOME]=mainWindow.webContents;
+//  browserWindows[pdfURLHOME]=mainWindow;  //don't hide the main window
   //https://github.com/atom/electron/blob/v0.34.3/docs/api/ipc-main-process.md#sending-messages
   //https://github.com/atom/electron/issues/3440
   //http://qiita.com/indometacin/items/018f78757c54a4c2eb5b
   ipcMain.on('retrieve', function(event, arg) {
-          console.log(arg);  // prints "ping"
+          console.log();  // prints "ping"
           console.log(path.join(__dirname, 'preload.js'));
           event.sender.send('asynchronous-reply', 'pong');
         });
-  webContents.send("incoming");//http://stackoverflow.com/questions/30681639/how-to-access-browserwindow-javascript-global-from-main-process-in-electron
+  ipcMain.on('retrieve2', function(event, arg) {
+          console.log(arg.pdfViewer._location);          console.log(arg.pdfViewer.linkService);          console.log(arg.url);  // prints "ping"          console.log(arg.pdfDocument.pdfInfo.fingerprint);  // prints "ping"
+          event.sender.send('asynchronous-reply', 'pong');
+        });
+  mainWindow.webContents.send("incoming");//http://stackoverflow.com/questions/30681639/how-to-access-browserwindow-javascript-global-from-main-process-in-electron
 //  webContents.executeJavaScript("getData();");
   //https://github.com/atom/electron/blob/master/docs/api/ipc-main.md
   mainWindow.on('closed', function() {
+    for (var mthefile in browserWindows){
+        browserWindows[mthefile].close();
+        delete browserWindows[mthefile];
+    };
+    delete webContents[pdfURLHOME];
     mainWindow = null;
   });
 });
-
 //____________________________________________________________________________________________
 //____________________________________________________________________________________________
-//____________________________________________________________________________________________
-//____________________________________________________________________________________________
-//____________________________________________________________________________________________
+var http = require('http');
+var url = require("url");
 
-var ipc=require('node-ipc');
+function copybmk(res,ourl) {
+  console.log("This is the copybmk page.");
+  Object.keys(webContents).map(function(v) { 
+    console.log(v); 
+    webContents[v].send("incoming2",v);
+    });
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.end("This is copybmk page.");
+}
+ 
+function gotobmk(res,ourl) {
+  console.log("This is the gotobmk page.");
+  var thefile=ourl.query['file'];
+  console.log('thefile: '+thefile);
+  if(!(thefile in webContents)){
+    var mainWindow = new BrowserWindow({
+        width: 800,
+        height: 1000,
+        webPreferences: {
+          nodeIntegration: false,
+          webSecurity: false,
+          preload: path.join(__dirname, 'preload.js')
+        },
+    });
+    //console.log(host+'/pdf.js/web/viewer.html'+ourl.search);//?' + param);
+    mainWindow.loadURL(decodeURI(host+'/pdf.js/web/viewer.html'+ourl.search));
+    webContents[thefile]=mainWindow.webContents;
+    browserWindows[thefile]=mainWindow;
+    mainWindow.on('closed', function() {
+        delete webContents[thefile];
+        delete browserWindows[thefile];
+    });
+    mainWindow.webContents.on('did-finish-load', function() {
+        mainWindow.webContents.send("incoming",thefile);//http://stackoverflow.com/questions/30681639/how-to-access-browserwindow-javascript-global-from-main-process-in-electron
+    });
+  }else{
+    webContents[thefile].send("incoming2",'page='+ourl.query['page']+'&zoom='+ourl.query['zoom']);
+  }
+  for (var mthefile in browserWindows){
+    browserWindows[mthefile].setAlwaysOnTop(false);
+    browserWindows[mthefile].hide();
+  };
+  browserWindows[thefile].setAlwaysOnTop(true);
+  browserWindows[thefile].show();
+  Object.keys(ourl.query).map(function(v) { console.log(v+':'+ourl.query[v]); });
+  Object.keys(webContents).map(function(v) { console.log(v); });
+//  webContents.send("incoming",res);
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.end("This is gotobmk page.");
+}
 
-/***************************************\
- *
- * You should start both hello and world
- * then you will see them communicating.
- *
- * *************************************/
+function route(pathname, res, ourl) {
+  var handle = {};
+  handle["/copybmk"] = copybmk;
+  handle["/gotobmk"] = gotobmk;
+  if (typeof handle[pathname] === 'function') {
+    return handle[pathname](res,ourl);
+  } else {
+    console.log("404 Not Found " + pathname);
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.end("404 Not Found " + pathname);
+  }
+}
 
-ipc.config.id = 'world';
-ipc.config.retry= 1500;
-ipc.config.maxConnections=1;
-
-ipc.serveNet(
-    function(){
-        ipc.server.on(
-            'message',
-            function(data,socket){
-                //webContents.executeJavaScript("getData();");
-                webContents.send("incoming");
-                ipc.log('got a message : '.debug, 'hi');//webContents);//.document.PDFViewerApplication);//PDFViewerApplication.pdfViewer.currentPageNumber);//data);
-                ipc.server.emit(
-                    socket,
-                    'message',
-                    data+' world!'
-                );
-            }
-        );
-
-        ipc.server.on(
-            'socket.disconnected',
-            function(data,socket){
-                console.log(arguments);
-            }
-        );
-    }
-);
-
-ipc.server.on(
-    'error',
-    function(err){
-        ipc.log('Got an ERROR!'.warn,err);
-    }
-);
-
-ipc.server.start();
+http.createServer(function (req, res) {
+    var ourl = url.parse(req.url,true);
+    route(ourl.pathname,res,ourl);
+}).listen(6968, '127.0.0.1');
--- preload.js
+++ preload.js
@@ -1,11 +1,25 @@
 ﻿var ipc = require("electron").ipcRenderer;
 //https://discuss.atom.io/t/ipc-send-dom-object/26554
-function getData(){
-  console.log("sent");
-  ipc.send("retrieve", PDFViewerApplication);//document);
+function setData(thefile){
+  console.log("sent "+thefile);
+  PDFViewerApplication.thefile=thefile;
+  console.log("PDFViewerApplication.thefile: "+PDFViewerApplication.thefile);
+//  ipc.send("retrieve2", PDFViewerApplication);//document);
 }
 
-ipc.on("incoming", function(event){
-  console.log("incoming");
-  getData();
+function setHash(hash){
+  console.log("hash "+hash);
+  PDFViewerApplication.pdfLinkService.setHash(hash);
+  console.log("PDFViewerApplication.pdfViewer._location.pdfOpenParams: "+PDFViewerApplication.pdfViewer._location.pdfOpenParams);
+//  ipc.send("retrieve2", PDFViewerApplication);//document);
+}
+
+ipc.on("incoming2", function(event,hash){
+  console.log("incoming2: "+hash);
+  setHash(hash);
+});
+
+ipc.on("incoming", function(event, thefile){
+  console.log("incoming "+thefile);
+  setData(thefile);
 });
