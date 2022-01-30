2dd7d97376990a674fbbcd18df71d9408a7aeb85
jawatech <jawatech@gmail.com>
2022-01-31 04:40:04 +0800

    stable 202201

--- main.js
+++ main.js
@@ -3,96 +3,239 @@ const qs = require ("querystring");
 const electron = require('electron');
 const path = require('path');
 //console.log(path.resolve(require('electron')));
-console.log(require.resolve('electron'));
+// console.log(require.resolve('electron'));
 const fs = require('fs');
-const app = electron.app;
+//const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 //const pdfURLHOME = "http://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf";
 //const pdfURLHOME = "file://C:/Users/sig/github/pdf.js/web/compressed.tracemonkey-pldi-09.pdf";
-const pdfURLHOME = "Dropbox/org/gtd/mygtd.pdf";
-const host='http://127.0.0.1:8080/Dropbox/';
+const pdfURLHOME = "Dropbox/org/gtd/mygtd.pdf";//\Dropbox\misc\Harrison’s Principles of Internal Medicine, 20th Edition.pdf
+//const pdfURLHOME = "Dropbox/misc/Harrison’s Principles of Internal Medicine, 20th Edition.pdf";//\Dropbox\misc\Harrison’s Principles of Internal Medicine, 20th Edition.pdf
+const host='http://127.0.0.1:8889/Dropbox/';
 const host2='http://127.0.0.1:6968/';
+//const viewerurl='pdf.js/build/generic/web/viewer.html';
+const viewerurl='jawatech/pdfjs2021/build/generic/web/viewer.html';
 var webContents = {};
 var browserWindows = {};
-var mainWindow = null;
+var mainWindow0 = null;
 const ipcMain = require('electron').ipcMain;
-
+const { app, Menu } = require('electron')
+const isMac = process.platform === 'darwin'
+//require('./menu');
 app.on('ready', function() {
-  mainWindow = new BrowserWindow({
+  mainWindow0 = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: false,
-      webSecurity: false,
+      webSecurity: true,
 	// Load `electron-notification-shim` in rendering view, by requiring it in your preloaded script. 
       //the path of preload script has to be "absolute path".
       //https://github.com/atom/electron/blob/v0.34.3/docs/api/browser-window.md#new-browserwindowoptions
       preload: path.join(__dirname, 'preload.js')
     },
   });
+  
   const param = qs.stringify({file: pdfURLHOME});
+  //console.log(host+'jawatech/pdfjs2021/build/generic/web/viewer.html?' + param);
   //mainWindow.loadURL('file://' + __dirname + '/../pdf.js/web/viewer.html?' + param);
   //file://C:\Users\sig\github\electron-pdfjs/../pdf.js/web/viewer.html
-  mainWindow.loadURL(host+'pdf.js/build/generic/web/viewer.html?' + param);
-  console.log(host+'pdf.js/build/generic/web/viewer.html?' + param);
+  //mainWindow.loadURL(host+'jawatech/pdfjs2021/build/generic/web/viewer.html?' + param);
+  mainWindow0.loadURL(host+'?frame');
+  //console.log(host+'jawatech/pdfjs2021/build/generic/web/viewer.html?' + param);
   //mainWindow.webContents.openDevTools();
-  webContents[pdfURLHOME]=mainWindow.webContents;
+  webContents[pdfURLHOME]=mainWindow0.webContents;
+  mainWindow0['thefile']=pdfURLHOME;
 //  browserWindows[pdfURLHOME]=mainWindow;  //don't hide the main window
   //https://github.com/atom/electron/blob/v0.34.3/docs/api/ipc-main-process.md#sending-messages
   //https://github.com/atom/electron/issues/3440
   //http://qiita.com/indometacin/items/018f78757c54a4c2eb5b
   ipcMain.on('retrieve', function(event, arg) {
-          console.log();  // prints "ping"
-          console.log(path.join(__dirname, 'preload.js'));
+          // console.log();  // prints "ping"
+          // console.log(path.join(__dirname, 'preload.js'));
           event.sender.send('asynchronous-reply', 'pong');
         });
   ipcMain.on('retrieve2', function(event, arg) {
-          console.log(arg.pdfViewer._location);          console.log(arg.pdfViewer.linkService);          console.log(arg.url);  // prints "ping"          console.log(arg.pdfDocument.pdfInfo.fingerprint);  // prints "ping"
+          // console.log(arg.pdfViewer._location);          console.log(arg.pdfViewer.linkService);          console.log(arg.url);  // prints "ping"          console.log(arg.pdfDocument.pdfInfo.fingerprint);  // prints "ping"
           event.sender.send('asynchronous-reply', 'pong');
         });
-  mainWindow.webContents.send("incoming");//http://stackoverflow.com/questions/30681639/how-to-access-browserwindow-javascript-global-from-main-process-in-electron
+  mainWindow0.webContents.send("incoming",'page=2');//http://stackoverflow.com/questions/30681639/how-to-access-browserwindow-javascript-global-from-main-process-in-electron
 //  webContents.executeJavaScript("getData();");
   //https://github.com/atom/electron/blob/master/docs/api/ipc-main.md
-  mainWindow.on('closed', function() {
+  mainWindow0.on('closed', function() {
     for (var mthefile in browserWindows){
         browserWindows[mthefile].close();
         delete browserWindows[mthefile];
     };
     delete webContents[pdfURLHOME];
-    mainWindow = null;
+    mainWindow0 = null;
   });
 });
+const template = [
+  // { role: 'appMenu' }
+  ...(isMac ? [{
+    label: app.name,
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideothers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  }] : []),
+  // { role: 'fileMenu' }
+  {
+    label: 'File',
+    submenu: [
+      isMac ? { role: 'close' } : { role: 'quit' }
+    ]
+  },
+  // { role: 'editMenu' }
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      ...(isMac ? [
+        { role: 'pasteAndMatchStyle' },
+        { role: 'delete' },
+        { role: 'selectAll' },
+        { type: 'separator' },
+        {
+          label: 'Speech',
+          submenu: [
+            { role: 'startSpeaking' },
+            { role: 'stopSpeaking' }
+          ]
+        }
+      ] : [
+        { role: 'delete' },
+        { type: 'separator' },
+        { role: 'selectAll' }
+      ])
+    ]
+  },
+  // { role: 'viewMenu' }
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forceReload' },
+      { role: 'toggleDevTools' },
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  },
+  // { role: 'windowMenu' }
+  {
+    label: 'Window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'zoom' },
+      ...(isMac ? [
+        { type: 'separator' },
+        { role: 'front' },
+        { type: 'separator' },
+        { role: 'window' }
+      ] : [
+        { role: 'close' }
+      ])
+    ]
+  },
+  {
+    label: 'PDF',
+    submenu: [
+      {
+        label: 'Copy link',
+        accelerator:'ctrl+F9',
+        click:function(){
+          var thefile=BrowserWindow.getFocusedWindow()['thefile'];
+          // console.log('copy current PDF view as bookmark:'+thefile);
+          BrowserWindow.getFocusedWindow().send("copyBookmark",thefile,'nullhash');
+          // Object.keys(webContents).map(function(v) { 
+          //   console.log("copyBookmark: v="+v+', thefile: '+thefile); 
+          //   webContents[v].send("copyBookmark",v,'nullhash');
+          //   });
+        }
+      }
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://electronjs.org')
+        }
+      }
+    ]
+  }
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
+
 //____________________________________________________________________________________________
 //____________________________________________________________________________________________
 var http = require('http');
 var url = require("url");
 
 function copybmk(res,ourl,req) {
-  console.log("This is the copybmk page.");
+  // console.log("This is the copybmk page.");
   Object.keys(webContents).map(function(v) { 
-    console.log(v); 
+    // console.log(v); 
     webContents[v].send("incoming2",v);
     });
-  res.writeHead(200, {"Content-Type": "text/plain"});
+  //res.writeHead(200, {"Content-Type": "text/plain","Access-Control-Allow-Origin": req.headers.origin});
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
   res.end("This is copybmk page.");
+}
+
+function copybmks(res,ourl,req) {
+  // console.log("This is the copybmks page. ourl="+ourl);
+  Object.keys(webContents).map(function(v) { 
+    // console.log("copybmks: v="+v); 
+    webContents[v].send("copybmks",v,'nullhash');
+    });
+  res.writeHead(200, {"Content-Type": "text/plain","Access-Control-Allow-Origin": req.headers.origin});
+//  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.end("This is copybmks page.");
 }
  
 function gotobmk(res,ourl,req) {
-  console.log("This is the gotobmk page.");
+  // console.log("This is the gotobmk page.");
   var thefile=ourl.query['file'];
-  console.log('thefile: '+thefile);
+  // console.log('thefile: '+thefile);
   if(!(thefile in webContents)){
     var mainWindow = new BrowserWindow({
         width: 1400,
         height: 2200,
         webPreferences: {
           nodeIntegration: false,
-          webSecurity: false,
+          webSecurity: true,
           preload: path.join(__dirname, 'preload.js')
         },
     });
-    console.log(host+'/pdf.js/build/generic/web/viewer.html'+'=='+ourl.search+'=='+ourl.hash);//?' + param);
-    mainWindow.loadURL(decodeURI(host+'pdf.js/build/generic/web/viewer.html'+ourl.search));
+    mainWindow['thefile']=thefile;
+    // console.log(host+viewerurl+'=='+ourl.search+'=='+ourl.hash);//?' + param);
+    if(ourl.search.startsWith('?file=/'))
+        mainWindow.loadURL(decodeURI(host+viewerurl+ourl.search));
+    else        
+        mainWindow.loadURL(decodeURI(host+viewerurl+'?file=/'+ourl.search.substring(6)));
     webContents[thefile]=mainWindow.webContents;
     browserWindows[thefile]=mainWindow;
     mainWindow.on('closed', function() {
@@ -101,10 +244,10 @@ function gotobmk(res,ourl,req) {
     });
     mainWindow.webContents.on('did-finish-load', function() {
         if(typeof ourl.query['page'] !== 'undefined'){
-            console.log("typeof ourl.query['page'] !== 'undefined'");
+            // console.log("typeof ourl.query['page'] !== 'undefined'");
             mainWindow.webContents.send("incoming",thefile,'page='+ourl.query['page']+'&zoom='+ourl.query['zoom']);//
         }else if(ourl.query['nameddest']!=''){
-            console.log("ourl.query['nameddest'] : "+ourl.query['nameddest']);
+            // console.log("ourl.query['nameddest'] : "+ourl.query['nameddest']);
             mainWindow.webContents.send("incoming",thefile,ourl.query['nameddest']);//            
         }
 //        mainWindow.webContents.send("incoming2",'page='+ourl.query['page']+'&zoom='+ourl.query['zoom']);
@@ -112,7 +255,7 @@ function gotobmk(res,ourl,req) {
     });//.finally(function() {webContents[thefile].send("incoming2",'page='+ourl.query['page']+'&zoom='+ourl.query['zoom']);});
   }else{
         if(typeof ourl.query['page'] !== 'undefined'){
-            console.log("typeof ourl.query['page'] !== 'undefined'");
+            // console.log("typeof ourl.query['page'] !== 'undefined'");
             webContents[thefile].send("incoming2",'page='+ourl.query['page']+'&zoom='+ourl.query['zoom']);
         }else if(ourl.query['explicitDest']　!== 'undefined'){
             webContents[thefile].send("incoming2",ourl.query['explicitDest']);
@@ -120,8 +263,8 @@ function gotobmk(res,ourl,req) {
             webContents[thefile].send("incoming2",ourl.query['nameddest']);
         }
   }
-  Object.keys(ourl.query).map(function(v) { console.log(v+':'+ourl.query[v]); });
-  Object.keys(webContents).map(function(v) { console.log(v); });
+  // Object.keys(ourl.query).map(function(v) { console.log(v+':'+ourl.query[v]); });
+  // Object.keys(webContents).map(function(v) { console.log(v); });
 //  webContents.send("incoming",res);
   res.writeHead(200, {"Content-Type": "text/plain"});
   res.end("This is gotobmk page.");
@@ -130,11 +273,12 @@ function gotobmk(res,ourl,req) {
 function route(pathname, res, ourl, req) {
   var handle = {};
   handle["/copybmk"] = copybmk;
+  handle["/copybmks"] = copybmks;
   handle["/gotobmk"] = gotobmk;
   if (typeof handle[pathname] === 'function') {
     return handle[pathname](res,ourl, req);
   } else {
-    console.log("404 Not Found " + pathname);
+    // console.log("404 Not Found " + pathname);
     res.writeHead(200, {"Content-Type": "text/plain"});
     res.end("404 Not Found " + pathname);
   }
@@ -144,3 +288,11 @@ http.createServer(function (req, res) {
     var ourl = url.parse(req.url,true);
     route(ourl.pathname,res,ourl,req);
 }).listen(6968, '127.0.0.1');
+// console.log();
+// console.log("### Starting local server");
+
+var WebServer = require("../jawatech/pdfjs2021/test/webserver.js").WebServer;
+var server = new WebServer();
+server.port = 8889;
+server.root = "../../../..";
+server.start();
--- preload.js
+++ preload.js
@@ -1,42 +1,118 @@
-﻿var ipc = require("electron").ipcRenderer;
+﻿//var ipc = ipcRenderer;//require("electron").ipcRenderer;
+var ipc = require("electron").ipcRenderer;
+//var PDFViewerApplication = require('pdf.js');
+//
+
+//var pdfjsLib = require('../jawatech/pdfjs2021/build/generic/build/pdf.js');
 //https://discuss.atom.io/t/ipc-send-dom-object/26554
 function setData(thefile, thehash){
   function wait(condition, callback) { //https://stackoverflow.com/questions/41328534/waiting-until-a-variable-exists-with-typeof-causes-an-infinite-loop
-      if ((typeof condition() !== "undefined")&&(typeof PDFViewerApplication.pdfViewer !== "undefined")&&(typeof PDFViewerApplication.pdfViewer.firstPagePromise !== "undefined")) {
-        let firstPagePromise = PDFViewerApplication.pdfViewer.firstPagePromise;
-          firstPagePromise.then(function () {
-              setTimeout(function () {
-                  callback();
-              }, 1000)
-          });
-        
-      } else {
-          setTimeout(function () {
-              wait(condition, callback);
-          }, 1000)
+      if ((typeof condition() !== "undefined")){//&&(typeof PDFViewerApplication.pdfViewer !== "undefined")&&(typeof PDFViewerApplication.pdfViewer.firstPagePromise !== "undefined")) {
+          callback();
+          return;
       }
+      alert('setTimeout');
+      setTimeout(function () {
+          wait(condition, callback);
+      }, 1000);
   }
-  console.log("thefile: "+thefile);
-  setTimeout(function () {wait(function(){if (typeof PDFViewerApplication!== "undefined"){return PDFViewerApplication;}else{return undefined;}}
-    , function(){setHash(thehash)})}, 1000);
-//  setTimeout(function(){console.log("PDFViewerApplication.thefile: "+PDFViewerApplication.thefile+', initialHash: '+PDFViewerApplication.initialHash);}, 1000)
-  
-//  ipc.send("retrieve2", PDFViewerApplication);//document);
+  // console.log("thefile: "+thefile);
+  setTimeout(function () {wait(
+        getPdfva
+    , function(){
+        getPdfva().initialBookmark = thehash;
+        })}, 1000);
+}
+function copyToClipboard(text) {//https://komsciguy.com/js/a-better-way-to-copy-text-to-clipboard-in-javascript/
+  const listener = function(ev) {
+    ev.preventDefault();
+    ev.clipboardData.setData('text/plain', text);
+  };
+  document.addEventListener('copy', listener);
+  document.execCommand('copy');
+  document.removeEventListener('copy', listener);
+}
+function copyBookmark() {
+  if(getWindow().getSelection().text=='')
+    result='[[pdfbmk:'+encodeURI(getPdfva().pdfDocumentProperties.url)+'&'+encodeURI(getPdfva().pdfViewer._location.pdfOpenParams.slice(1))+']]';
+  else
+    result='[[pdfbmk:'+encodeURI(getPdfva().pdfDocumentProperties.url)+'&'+encodeURI(getPdfva().pdfViewer._location.pdfOpenParams.slice(1))+']['+getWindow().getSelection()+']]';
+  // console.log(result);
+  copyToClipboard(result);
 }
 
 function setHash(thehash){
-  console.log("thehash "+thehash);
+  // console.log("thehash "+thehash);
   PDFViewerApplication.pdfLinkService.setHash(thehash);
 //  console.log("PDFViewerApplication.pdfViewer._location.pdfOpenParams: "+PDFViewerApplication.pdfViewer._location.pdfOpenParams);
 //  ipc.send("retrieve2", PDFViewerApplication);//document);
 }
 
 ipc.on("incoming2", function(event,thehash){
-  console.log("incoming2: "+thehash);
+  // console.log("incoming2: "+thehash);
   setHash(thehash);
 });
 
 ipc.on("incoming", function(event, thefile, thehash){
-  console.log("incoming "+thefile+", hash: "+thehash);
+  // console.log("incoming "+thefile+", hash: "+thehash);
   setData(thefile, thehash);
+});
+ipc.on("copyBookmark", function(event, thefile, thehash){
+  // console.log("copyBookmark ");
+  copyBookmark();
+});
+
+function getPdfOutlines(pdfva){
+    var result="* outline\n";
+     /**
+     * Use recursive method to traverse the first orderDOMtree
+     * @param node root node
+     */
+    function traversal(node,level){
+      //YesnodeTreatment
+      //result=result+','+('*'.repeat(level))+' '+node.tagName+((node.tagName=='DIV')?'('+node.className+')':'')+':'+node.children.length+"\n";
+      if(node.tagName=='A'){
+        result=result+('*'.repeat(1+level/2))+' '+node.text+"\n";
+        const url = new URL(node.href);
+        var path = url.search.split('&')[0].split('=')[1];
+        result+='[[pdfbmk:'+path+'&';
+        result+=url.hash.substring(1)+']['+node.text+"]]\n";
+      }
+      var i = 0, children = node.children,item;
+      for(; i < children.length ; i++){
+        item = children[i];
+        if(item.nodeType === 1){
+          //Recursive traversal sub node
+          traversal(item,level+1);
+        }
+      }
+    }
+    traversal(pdfva.pdfOutlineViewer.container,0);
+    var OutlinesText=result;
+    return OutlinesText;
+}
+function testpreload(){
+    alert('testpreload')
+}
+function getPdfva(){
+  pdfva=null;
+  if(typeof PDFViewerApplication== "undefined"){
+    pdfva=frames[0].window.PDFViewerApplication;}
+  else{pdfva=PDFViewerApplication;}
+  return pdfva;
+}
+function getWindow(){
+  if(typeof frames[0]== "undefined"){
+    return window;
+  }
+  else return frames[0].window;
+}
+ipc.on("copybmks", function(event, thefile, thehash){
+//  const { PDFViewerApplication, PDFViewer } = require('../jawatech/pdfjs2021/build/generic/web/viewer.js')
+  var pdfva=getPdfva();
+  // console.log("copybmks called on: "+thefile+", hash: "+thehash+', PDFViewerApplication: '+((typeof PDFViewerApplication== "undefined")?'undefined':'defined'));
+  // console.log('PDFViewerApplication: '+getPdfOutlines(pdfva));
+  copyToClipboard(getPdfOutlines(pdfva));
+//  console.log('pdfViewer: '+pdfViewer);
+//  setData(thefile, thehash);
 });
